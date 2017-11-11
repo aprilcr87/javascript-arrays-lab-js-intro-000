dc25a2c0f12501d4df168995efbc8d629f5df79f
April Rivera <aprilcr87@gmail.com>
2017-11-11 15:14:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ kittens.push(name);
 return kittens;
 }
 destructivelyAppendKitten('Nala');
+
+var kittens = ['Milo', 'Garfield', 'Otis'];
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+destructivelyPrependKitten('Cowboy');
